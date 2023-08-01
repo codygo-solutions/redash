1ea4ebc49157b63a57e70f327b8f01fc40106beb
kevinand11 <kevinfizu@gmail.com>
2023-08-01 11:59:11 +0100

    connect to aws ecr

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
 
 remote_address="ec2-54-93-179-229.eu-central-1.compute.amazonaws.com"
-image_name="kevinand11/redash"
-version=$(docker images | awk '($1 == "kevinand11/redash") {print $2 += .01; exit}')
+local_image_name="a9e/redash"
+remote_image_scope="public.ecr.aws/l5l8e4q8"
+remote_image_name="$remote_image_scope/redash"
+version=$(docker images | awk '($1 == "a9e/redash") {print $2 += .1; exit}')
 
-docker build --platform=linux/amd64 -t "$image_name:$version" .
-docker tag $image_name:"$version" $image_name:latest
+# authenticate docker with aws details
+aws ecr-public get-login-password --region us-east-1 --profile "$AWS_PROFILE" | docker login --username AWS --password-stdin $remote_image_scope
 
-docker push $image_name:"$version"
-docker push $image_name:latest
+docker build --platform=linux/amd64 -t "$local_image_name:$version" .
+docker tag $local_image_name:"$version" $remote_image_name:latest
+
+docker push $remote_image_name:"$version"
+docker push $remote_image_name:latest
 
 ssh -i deploy-keys.pem -T ubuntu@$remote_address <<'EOL'
 	cd /opt/redash
-	sudo docker image pull kevinand11/redash:latest
+	sudo docker image pull public.ecr.aws/l5l8e4q8/redash:latest
 	sudo docker-compose down
 	sudo docker-compose up -d
 EOL
