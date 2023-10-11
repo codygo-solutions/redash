a3465058c6240fa1c6f99f6c626dc5df23c63572
kevinand11 <kevinfizu@gmail.com>
2023-10-11 09:44:20 +0100

    support drilldown urls if in dashboard page

--- client/app/components/ApplicationArea/navigateTo.js
+++ client/app/components/ApplicationArea/navigateTo.js
@@ -9,6 +9,16 @@ export default function navigateTo(href, replace = false, target = undefined) {
   setTimeout(() => {
     const isExternal = stripBase(href) === false;
     if (isExternal) {
+      const isIframe = window.self !== window.top;
+      const parentLocation = new URL(isIframe ? document.referrer : document.location.href);
+      const urlLocation = new URL(href);
+      const dashboardRegex = /\/dashboards\/(.*?)$/;
+      const parentMatch = dashboardRegex.exec(parentLocation.pathname);
+      const urlMatch = dashboardRegex.exec(urlLocation.pathname);
+      if (parentMatch && urlMatch && parentLocation.origin === urlLocation.origin) {
+        const id = urlMatch[1]
+        return window.open(`${parentLocation.href}/${id}`, target)
+      }
       window.open(href, target);
       return;
     }
